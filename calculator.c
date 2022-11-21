c1842cfab6f92ae867c63942fab5a87e00d002bd
Devansh Ramdurgekar <devanshr3012@gmail.com>
2022-11-21 13:43:42 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -3,18 +3,29 @@
 int main(){
 	int num1,num2;
 	float result;
-	char choice;
+	int choice;
 	printf("Enter two numbers");
 	scanf("%d%d",&num1,&num2);
-	printf("Choose operation + - x / ");
-	scanf("%c\n",&choice);
-	if (choice == '+'){
+	printf("Calculator\nChoose 1 for Addition\nChoose 2 for Subtraction\nChoose 3 for Multiplication\nChoose 4 for Division");
+	scanf("%d",&choice);
+	printf("%d",choice);
+	
+	if (choice == 1){
 		result = num1+num2;
 	}
-	else {
-		result = 0;
+	if (choice == 2){
+		result = num1-num2;
 	}
-	printf("%c",choice);
+	if (choice == 3){
+		result = num1*num2;
+	}
+	if (choice == 4){
+		result = num1/num2;
+	}
+
+	else {
+		result = 69;
+	}
 	printf("%f",result);
 	return 0;
 }
