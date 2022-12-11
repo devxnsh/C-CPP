a730bd4a9bdc405678f8fc3f9283d706776c1db6
Devansh Ramdurgekar <devanshr3012@gmail.com>
2022-12-11 23:28:57 +0530

    Create seriessum.c

---
+++ seriessum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+int x,num=0,lim;
+float term,sum;
+printf("Enter X");
+scanf("%d",&x);
+
+printf("Enter Limit of summation");
+scanf("%d",&lim);
+while (num=lim)
+{
+term = (pow(x,num))/(pow(num,num));
+printf("%f\n",term);
+if (num%2==0){sum+=term;}
+
+else{sum-=term;}
+num++;
+printf("Result is, %f\n",sum);
+
+}
+printf("Result is, %f",sum);
+return 0;
+}
